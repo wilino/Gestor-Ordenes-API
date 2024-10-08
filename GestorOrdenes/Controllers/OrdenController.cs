17bb4c80f6f971380ebff79af123fab50fb738a8
Wilfredo Marcelino Yupanqui Villagaray <76481970+wilino@users.noreply.github.com>
2024-10-08 03:13:26 -0400

    Add files via upload

---
+++ GestorOrdenes/Controllers/OrdenController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using GestorOrdenes.Servicios.Interfaces;
+using GestorOrdenes.Servicios.DTOs;
+using GestorOrdenes.Servicios.Excepciones;
+
+namespace GestorOrdenes.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdenController : ControllerBase
+    {
+        private readonly IOrdenService _ordenService;
+
+        public OrdenController(IOrdenService ordenService)
+        {
+            _ordenService = ordenService;
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> ObtenerOrdenPorId(int id)
+        {
+            try
+            {
+                var orden = await _ordenService.ObtenerPorId(id);
+                return Ok(orden);
+            }
+            catch (OrdenNoEncontradaException ex)
+            {
+                return NotFound(ex.Message); // 404 Not Found
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error interno del servidor: {ex.Message}"); 
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CrearOrden([FromBody] CrearOrdenDto ordenDto)
+        {
+            try
+            {
+                await _ordenService.CrearOrden(ordenDto);
+                return Ok("Orden creada exitosamente."); 
+            }
+            catch (ClienteNoEncontradoException ex)
+            {
+                return NotFound(ex.Message); 
+            }
+            catch (ProductoNoEncontradoException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (StockInsuficienteException ex)
+            {
+                return BadRequest(ex.Message); 
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error interno del servidor: {ex.Message}"); 
+            }
+        }
+    }
+
+}
+
---
+++ GestorOrdenes/Program.cs
@@ -0,0 +1,51 @@
+﻿using GestorOrdenes.Dominio.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using GestorOrdenes.Datos;
+using GestorOrdenes.Datos.Repositorios;
+using GestorOrdenes.Servicios.Interfaces;
+using GestorOrdenes.Servicios.Servicios;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("GestorOrdenesDB")
+           .ConfigureWarnings(warnings => warnings.Ignore(InMemoryEventId.TransactionIgnoredWarning)));
+
+
+
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+builder.Services.AddScoped<IProductoRepository, ProductoRepository>();
+builder.Services.AddScoped<IOrdenRepository, OrdenRepository>();
+
+
+builder.Services.AddScoped<IOrdenService, OrdenService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Precargar datos al iniciar la aplicación
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    CargadorDatos.Precargar(services);
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
